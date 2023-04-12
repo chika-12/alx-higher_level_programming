5471fdf16b8987d02f674ced630b89f15775ddd5
chika-12 <markworship001@gmail.com>
2023-04-12 02:30:39 +0100

    python

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """checking if object is the instance of the specified class"""
-    if not isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return (type(obj) == a_class)
