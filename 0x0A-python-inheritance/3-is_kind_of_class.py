cfd584c0b2810a58e7ef77023304a4bf3c9b3d55
chika-12 <markworship001@gmail.com>
2023-04-12 03:01:42 +0100

    python

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function definition"""
+
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if object is an instance of the class"""
+    return(isinstance (obj, a_class))
