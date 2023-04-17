9983b939f05074a9248d39653b35f512f15ae6f8
chika-12 <markworship001@gmail.com>
2023-04-17 12:37:53 +0100

    python

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,8 +2,9 @@
 """A class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
-    """Creates a class"""
+    """Creates a class of instances"""
     def __init__(self, size):
         """Initiates a class"""
         self.integer_validator("size", size)
