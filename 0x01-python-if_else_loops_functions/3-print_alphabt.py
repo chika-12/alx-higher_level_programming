a675369ff79745cb1fc3c90a8abe495b449f8186
chika-12 <markworship001@gmail.com>
2024-03-12 15:56:06 +0100

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z') + 1):
     if chr(alphabet) != 'e' and chr(alphabet) != 'q':
         print("{}".format(chr(alphabet)), end="")
