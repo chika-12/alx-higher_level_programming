6800a16955abd25767ab4e227eed2cad86193149
chika-12 <markworship001@gmail.com>
2024-03-14 17:01:18 +0100

    module

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of all the command line arguments"""
+    import sys
+
+    count = len(sys.argv) - 1
+    sum = 0
+
+    for i in range(count):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
