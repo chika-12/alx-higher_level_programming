79f216770491d93cdf1d33de44e5c568e8004858
chika-12 <markworship001@gmail.com>
2024-03-14 16:16:32 +0100

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    """Prints the number of arguments in a list"""
+    import sys
 
-argv = sys.argv
-n = 1
-print("{} arguments:".format(len(argv) - 1))
-for val in argv:
-    if val != argv[0]:
-        print("{}: {}".format(n, val))
-        n += 1
+    argv = sys.argv
+    n = 1
+    if len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+
+    for val in argv:
+        if val != argv[0]:
+            print("{}: {}".format(n, val))
+            n += 1
