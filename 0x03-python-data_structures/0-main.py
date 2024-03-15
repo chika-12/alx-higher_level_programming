4985b3b574d6db9243afb1e59212bf20d9125f29
chika-12 <markworship001@gmail.com>
2024-03-15 17:43:02 +0100

    list

---
+++ 0x03-python-data_structures/0-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+new_list = [2,6,8,9,2,1]
+print_list_integer(my_list)
+print_list_integer(new_list)
+
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This project deals with Lists in python and Tuples
