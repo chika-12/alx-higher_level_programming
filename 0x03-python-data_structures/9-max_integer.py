f3713e53dca1ccbadd34c45fb540ab28ef5de147
chika-12 <markworship001@gmail.com>
2024-03-16 20:43:18 +0100

    Tuple

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = my_list[0]
-    count = len(my_list)
+    if (my_list == None):
+        return None
+    else:
+        max_num = my_list[0]
+        count = len(my_list)
 
-    for num in range(count):
-        if my_list[num] > max_num:
-            max_num = my_list[num]
+        for num in range(count):
+            if my_list[num] > max_num:
+                max_num = my_list[num]
     return max_num
