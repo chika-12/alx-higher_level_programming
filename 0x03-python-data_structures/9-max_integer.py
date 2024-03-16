f88c51ef9e994e932240855c202814b3950dc694
chika-12 <markworship001@gmail.com>
2024-03-16 21:50:54 +0100

    List

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (my_list == None):
-        return None
-    else:
+    if (my_list):
         max_num = my_list[0]
         count = len(my_list)
 
         for num in range(count):
             if my_list[num] > max_num:
                 max_num = my_list[num]
+    else:
+        return None
     return max_num
