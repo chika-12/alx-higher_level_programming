6b1cbe835c1bfa0236bd8e67dc19affb0add5841
chika-12 <markworship001@gmail.com>
2024-03-23 20:59:10 +0100

    new

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
     else:
         new = a_dictionary[next(iter(a_dictionary))]
         if len(a_dictionary) < 2:
-            first_key = next(iter(my_dict))
+            first_key = next(iter(a_dictionary))
             return first_key
         for k, v in a_dictionary.items():
             if a_dictionary[k] > new:
