29b0adcbaeebee3f40c4a14bcb7ec9fb07acb625
chika-12 <markworship001@gmail.com>
2024-03-24 01:29:40 +0100

    code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    if roman_string == None:
+        return None
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     add = 0
