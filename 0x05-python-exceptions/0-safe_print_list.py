e3e27d91b9a2b79743d6269d82847e7b8b9ca1d7
chika-12 <markworship001@gmail.com>
2024-04-02 12:05:23 +0100

    python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for num in range(x):
-            print(f"{my_list[num]}",end="")
+            print(f"{my_list[num]}", end="")
             count += 1
     except Exception:
         pass
