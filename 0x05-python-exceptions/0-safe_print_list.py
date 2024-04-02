ff99e77b6ec892192bdce48658ae097b713e92e6
chika-12 <markworship001@gmail.com>
2024-04-02 11:00:31 +0100

    python

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for num in range(x):
+            print(f"{my_list[num]}",end="")
+            count += 1
+    except Exception:
+        pass
+    print()
+    return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This repo explores the python exception course
