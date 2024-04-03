32a93079915fb20341bf7805caabda6bab57fde5
chika-12 <markworship001@gmail.com>
2024-04-03 09:00:31 +0100

    python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,8 @@ class Square:
     """This script does more computations on the square class"""
 
     def __init__(self, size=0):
-        self.__size = int(size)
-
         if not isinstance(size, int):
             raise TypeError("size must be an interger")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
