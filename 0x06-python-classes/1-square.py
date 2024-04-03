f3ac1bb59ce2d85c73ebd5c3067100856e698a69
chika-12 <markworship001@gmail.com>
2024-04-03 08:25:03 +0100

    python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A python script"""
+
+
+class Square:
+
+    """Defining square for easy computation involving
+    a square computions like finding the size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
