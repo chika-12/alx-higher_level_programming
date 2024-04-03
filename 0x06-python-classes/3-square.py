e5b19e3e836d3a36c960db127365866cadcba390
chika-12 <markworship001@gmail.com>
2024-04-03 09:12:19 +0100

    python

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is a python script"""
+
+
+class Square:
+
+    """This script does more computations on the square class"""
+
+    def __init__(self, size=0):
+
+        """Defines the size of a square
+        raises: an error message if size is not an int
+        an also when it less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        return self.__size * self.__size
