fb838053c487010204c04a3bf16ab02e17b3735a
chika-12 <markworship001@gmail.com>
2024-04-03 08:13:12 +0100

    python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""A python script"""
+
+
 class Square:
-    """This lines creates an empty square for use
-    """
+
+    """This lines creates an empty square for use"""
+
     pass
