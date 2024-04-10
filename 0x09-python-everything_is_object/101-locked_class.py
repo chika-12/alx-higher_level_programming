9873b60af9450b218e58fd46d6c333fc6b9ec50b
chika-12 <markworship001@gmail.com>
2024-04-10 13:56:50 +0100

    python

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,7 +9,4 @@ class LockedClass:
     A locked class attribute
     """
 
-    def __setattr__(self, attr, value):
-        if not hasattr(self, attr) and attr != 'first_name':
-            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attr}'")
-        super().__setattr__(attr, value)
+    __slots__ = ["first_name"]
