de65ee808f2c547ac622d38b929d1edf479e85bd
chika-12 <markworship001@gmail.com>
2024-04-16 02:38:16 +0100

    python

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A python code"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """raises an exception if area is not implemented"""
+
+        raise Exception("area() is not implemented")
