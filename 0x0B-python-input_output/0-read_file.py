5ecb6135ebdea41358c08b2d3ce88a5a9b8bdc45
chika-12 <markworship001@gmail.com>
2024-04-16 15:49:21 +0100

    python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """A python code"""
 
+
 def read_file(filename=""):
     """opening file for reading"""
 
     with open(filename, "r", encoding="UTF-8") as file:
         line = file.read()
-        
+
         print(line)
     file.close()
