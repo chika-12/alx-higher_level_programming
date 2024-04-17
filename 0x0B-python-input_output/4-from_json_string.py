424b3eb628eef81e16c5df1165aadc372f43e8e2
chika-12 <markworship001@gmail.com>
2024-04-17 04:37:46 +0100

    code

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Apython code written by chika"""
+import json
+
+
+def from_json_string(my_str):
+    """"Retrives a python dictionary from a json format"""
+
+    return (json.loads(my_str))
