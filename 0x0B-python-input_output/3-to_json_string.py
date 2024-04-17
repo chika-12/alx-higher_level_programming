6eb3a1faef6ce5e1ddbd9f47e5b7562a0922093f
chika-12 <markworship001@gmail.com>
2024-04-17 04:31:26 +0100

    python

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A python code written by chika mark"""
+import json
+
+
+def to_json_string(my_obj):
+    """coverting a python dictionary to json format for easy file processing"""
+
+    return (json.dumps(my_obj))
