d4cedb79d5b7eb65f659352139c73210fc233a3f
chika-12 <markworship001@gmail.com>
2024-06-19 20:26:26 +0100

    script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length > 2) {
-  argv.forEach((val, index) => {
-    if (index > 1) {
-      console.log('Argument found');
-    }
-  });
-} else {
+if (argv.length === 2) {
   console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
