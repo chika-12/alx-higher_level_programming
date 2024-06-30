adb5773e309d30b79c2fe8ae69caf0dbe3c2d290
chika-12 <markworship001@gmail.com>
2024-06-30 02:38:15 +0100

    script

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let n = 0; n < this.height; n++) {
+      for (let x = 0; x < this.width; x++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
